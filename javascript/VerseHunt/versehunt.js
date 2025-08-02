9ffd0b52bc5f6f37ca61c7da55631a487effd5d2
Ruhama Fikre <ruhamis.projects985@gmail.com>
2025-08-02 14:19:14 +0300

    wrote basic verse skeleton

---
+++ javascript/VerseHunt/versehunt.js
@@ -0,0 +1,4 @@
+//elements declaration
+const quargle = document.querySelector('.quargle');
+const verse = document.querySelector('.verse');
+const headerTabs = document.querySelector('.header-browser-tabs');
